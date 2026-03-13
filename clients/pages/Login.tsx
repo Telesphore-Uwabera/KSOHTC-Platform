87091c7e88550c8400b2b341d8b56397e05ad8a1
Telesphore Uwabera <t.uwabera@alustudent.com>
2026-03-13 11:20:11 +0200

    :

--- clients/pages/Login.tsx
+++ clients/pages/Login.tsx
@@ -143,12 +143,6 @@ export default function Login() {
                     View courses
                   </Link>
                 </p>
-                <p className="text-center text-gray-500 text-xs mt-1">
-                  Administrator?{" "}
-                  <Link to="/admin/login" className="font-medium text-primary hover:underline">
-                    Sign in at /admin/login
-                  </Link>
-                </p>
               </div>
             </div>
           </div>
